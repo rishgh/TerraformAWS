a05cd8e939bfddf338d0ede9b79aa6672247bd48
rishgh <36372417+rishgh@users.noreply.github.com>
2018-06-21 08:23:13 -0500

    Add files via upload

---
+++ ec2-machines.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "app" {
+  ami           = "${lookup(var.AmiLinux, var.region)}"
+  instance_type = "t2.micro"
+  associate_public_ip_address = "true"
+  subnet_id = "${aws_subnet.PublicAZA.id}"
+  vpc_security_group_ids = ["${aws_security_group.FrontEnd.id}"]
+  key_name = "${var.key_name}"
+  tags {
+        Name = "app"
+  }}
